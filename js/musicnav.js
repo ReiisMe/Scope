99284f0b5c4bb8b420bfcb918907ffd385c5f248
yuw446 <43831158+yuw446@users.noreply.github.com>
2018-11-08 23:05:17 -0800

    Added Functionality to the core feature
    Implemented the core feature

--- js/musicnav.js
+++ js/musicnav.js
@@ -58,3 +58,12 @@ function openTab(evt, platformName) {
     document.getElementById(platformName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+
+
+
+function getPost(){
+	$('#loadmorebutton').remove();
+	$('#Facebook').append('<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fapplemusic%2Fposts%2F2656644727684561%3A0&width=500" width="500" height="632" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>');
+	$('#Facebook').append('<p><button class="btn btn-outline-primary btn-lg" onclick="getPost()" id="loadmorebutton">See More</button></p>');
+}
