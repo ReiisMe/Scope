34c7b546d3d50f5df49d4f17a3939effc09e1c68
yuw446 <43831158+yuw446@users.noreply.github.com>
2018-11-26 21:00:29 -0800

    functionality update - afterlogin

--- js/login.js
+++ js/login.js
@@ -1,22 +1,22 @@
 function login() {
- 
+
     var username = document.getElementById("username");
     var pass = document.getElementById("password");
     if (username.value == "") {
- 
+
         alert("enter your username");
- 
+
     } else if (pass.value  == "") {
- 
+
         alert("enter your password");
- 
+
     } else if(username.value == "kelly" && pass.value == "123456"){
- 
-        window.location.href="../index.html?username="+username.value;
- 
+
+        window.location.href="../index-afterlogin.html?username="+username.value;
+
     } else {
- 
+
         alert("please enter the correct username or password")
- 
+
     }
 }
